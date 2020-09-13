b8d12cc564be8c1ff32eff2385019f543ecf5c11
Ain Mor <ainbmor@gmail.com>
2020-09-13 14:05:44 +0200

    Added all CRUD operations

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -40,9 +42,9 @@ namespace API
                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
                 });
             });
-            services.AddControllers();
 
-            //services.AddMvcCore.SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
